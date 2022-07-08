f3a3d7ee373f858eabae8c65971640f70976fef0
Gizachew Tilahun <69030424+Gizachew-tilahun@users.noreply.github.com>
2022-07-08 16:34:44 +0300

    Update mutations.py

--- source code/Python/codee1/mutations.py
+++ source code/Python/codee1/mutations.py
@@ -1,11 +1,8 @@
-# Mutations in Python - HackerRank Solution
 def mutate_string(string, position, character):
-    # Mutations in Python - HackerRank Solution START
     l = list(string)
     l[position] = character;
     string = ''.join(l);
     return string
-    # Mutations in Python - HackerRank Solution END
 if __name__ == '__main__':
     s = input()
     i, c = input().split()
